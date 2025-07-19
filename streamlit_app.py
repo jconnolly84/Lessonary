1f2ba8a03d189e7ce726b5d7a7231851361f33ef
jconnolly84 <75783119+jconnolly84@users.noreply.github.com>
2025-07-19 19:41:45 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,16 @@
 import streamlit as st
 from auth_utils import (
-    get_google_auth_url,
-    get_microsoft_auth_url,
-    handle_google_callback,
-    handle_microsoft_callback,
+    login_with_google,
+    login_with_microsoft,
+    handle_callback,
 )
-from lessonary_ui import render_lessonary_ui
+from lessonary_ui import show_welcome_ui
 
 st.set_page_config(page_title="Lessonary", page_icon="📘", layout="centered")
 
-def handle_callback():
-    query_params = st.query_params
-    if "code" in query_params:
-        provider = st.session_state.get("oauth_provider", "google")
-        code = query_params["code"]
-        if provider == "google":
-            handle_google_callback(code)
-        elif provider == "microsoft":
-            handle_microsoft_callback(code)
-        st.rerun()
-
 handle_callback()
 
-if "user" in st.session_state:
+if "user_info" in st.session_state:
     render_lessonary_ui()
 else:
     st.image("assets/lessonary_logo.png", width=200)
@@ -32,11 +20,9 @@ else:
     with col1:
         st.image("assets/google_logo.png", width=40)
         if st.button("Login with Google"):
-            st.session_state['oauth_provider'] = 'google'
-            st.switch_page(get_google_auth_url())
+            login_with_google()
 
     with col2:
         st.image("assets/microsoft_logo.png", width=40)
         if st.button("Login with Microsoft"):
-            st.session_state['oauth_provider'] = 'microsoft'
-            st.switch_page(get_microsoft_auth_url())
+            login_with_microsoft()
