2c967540d36add23825b6b3a820dffdf9f303e41
jconnolly84 <75783119+jconnolly84@users.noreply.github.com>
2025-07-19 22:00:48 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,45 +3,22 @@ import streamlit as st
 from auth_utils import (
     login_with_google,
     login_with_microsoft,
-    handle_callback,
-)
-from lessonary_ui import render_lessonary_ui
-
-# Break out of iframe sandbox (needed for Google/Microsoft auth to work properly)
-st.components.v1.html(
-    """
-    <script>
-        if (window.top !== window.self) {
-            window.top.location = window.location.href;
-        }
-    </script>
-    """,
-    height=0,
+    handle_google_auth_callback,
+    handle_microsoft_auth_callback
 )
 
-st.set_page_config(page_title="Lessonary", page_icon="📘", layout="centered")
+# Always show the login screen for testing
+st.image("assets/lessonary_logo.png", width=200)
+st.title("Login to Lessonary")
 
-handle_callback()
+if st.button("Login with Google"):
+    login_with_google()
+    st.experimental_rerun()
 
-if "user_info" in st.session_state:
-    render_lessonary_ui()
-else:
-    st.image("assets/lessonary_logo.png", width=200)
-    st.title("Login to Lessonary")
+if st.button("Login with Microsoft"):
+    login_with_microsoft()
+    st.experimental_rerun()
 
-    if st.button("Login with Google"):
-        login_with_google()
-
-    if st.button("Login with Microsoft"):
-        login_with_microsoft()
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image("assets/google_logo.png", width=40)
-        if st.button("Login with Google"):
-            login_with_google()
-
-    with col2:
-        st.image("assets/microsoft_logo.png", width=40)
-        if st.button("Login with Microsoft"):
-            login_with_microsoft()
+# Callback handler (if redirected back with code)
+handle_google_auth_callback()
+handle_microsoft_auth_callback()
